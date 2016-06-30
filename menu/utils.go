64524be9b39074f1a066e049702f795a878be291
Austin Dizzy <dizzy@wow.com>
2016-06-30 12:33:13 -0400

    Added more helper funcs to menu/utils
    * Added GetRepo, to prompt the user to select a
    repository in the system
    * Added GetWriter to prompt the user to select
    a location to write the csv.Writer to (e.g. csv
    file location)
    * Added GetFolder to prompt the user for a valid
    directory location

--- menu/utils.go
+++ menu/utils.go
@@ -2,9 +2,16 @@ package menu
 
 import (
 	"bufio"
+	"encoding/csv"
 	"fmt"
 	"os"
 	"strings"
+	"text/tabwriter"
+
+	"github.com/austindizzy/securitycenter-cli/api"
+	"github.com/austindizzy/securitycenter-cli/auth"
+	"github.com/austindizzy/securitycenter-cli/utils"
+	"github.com/urfave/cli"
 )
 
 // GetInput gets a user-supplied string using the supplied
@@ -21,6 +28,22 @@ func GetInput(msg string) string {
 	fmt.Println()
 
 	return input
+}
+
+// GetWriter gets a csv.Writer for a CSV file specified by the user, used
+// for determining where to write exported records to.
+func GetWriter(c *cli.Context) *csv.Writer {
+	var (
+		filePath  = GetInput("Please enter location to save export (.csv)")
+		file, err = os.Create(filePath)
+		w         *csv.Writer
+	)
+
+	utils.LogErr(c, err)
+
+	w = csv.NewWriter(file)
+
+	return w
 }
 
 // GetSelection gets a user-supplied selection used for traversing menus
@@ -47,4 +70,61 @@ func GetSelection(def ...string) string {
 	}
 
 	return selection
+}
+
+// GetFolder gets the path to a user supplied folder, used for exporting
+// a number of files to (for instance, report .xml files, .nessus scan results, etc).
+func GetFolder() string {
+	var (
+		t    = false
+		path string
+		err  error
+	)
+
+	for t == false {
+		path = GetInput("Enter a directory for exported file(s)")
+		_, err = os.Stat(path)
+		if err == nil {
+			t = true
+		}
+		if os.IsNotExist(err) {
+			fmt.Printf("The directory \"%s\" does not exist. Please try again.\n", path)
+		}
+	}
+
+	return path
+}
+
+// GetRepo fetches a list of all the repositories in the system and displays
+// them as a choose list for batch importing various objects which require a user-selected repository.
+func GetRepo(c *cli.Context) (string, error) {
+	var (
+		keys, err  = auth.Get(c)
+		w          = new(tabwriter.Writer)
+		repoFilter = map[string]interface{}{
+			"fields": "id,name",
+		}
+		repoRes *api.Result
+	)
+
+	if err != nil {
+		return "", err
+	}
+
+	w.Init(os.Stdout, 0, 8, 0, '\t', 0)
+	repoRes, err = api.NewRequest("GET", "repository", repoFilter).WithAuth(keys).Do(c)
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Println("Repositories available in SecurityCenter:")
+	for i, r := range repoRes.Data.Get("response").MustArray() {
+		str := "[%d] %s\t"
+		if i%3 == 0 {
+			str += "\n"
+		}
+		fmt.Fprintf(w, str, i, r.(map[string]interface{})["name"])
+	}
+
+	return GetSelection(), nil
 }
